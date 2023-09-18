fccda85ec17fe778cb7d84888af9a7046be163d4
Александр Колосов <dnwSilver@gmail.com>
2023-09-18 15:05:31 +0500

    Add alias for release.

--- releases.sh
+++ releases.sh
@@ -2,9 +2,18 @@ RED="\e[91m"
 GREEN="\e[32m"
 BLUE="\e[94m"
 NC="\e[0m"
+YELLOW="\e[33m"
 
 function create_and_push_tag ()
 {
+  git switch $(git_main_branch) > /dev/null 2>&1 || return
+
+  echo -e " ${GREEN}󱓏${NC} Swap to branch ${GREEN}$(git_main_branch)${NC}." 
+  
+  git pull origin $(current_branch) > /dev/null 2>&1 || return
+  git fetch --all --prune --jobs=10 > /dev/null 2>&1 || return
+  echo -e " ${GREEN}󱓍${NC} Pull and fetch from branch ${GREEN}$(current_branch)${NC}." 
+
   CURRENT_VERSION=$(get_version_typescript)
   if [ $(git tag -l "v$CURRENT_VERSION") ]; then
     echo -e " ${BLUE}󰜣${NC} ${RED}Tag v${CURRENT_VERSION} already created${NC}."
@@ -20,8 +29,7 @@ function create_and_push_tag ()
 function release_create ()
 {
   git switch $(git_develop_branch) > /dev/null 2>&1 || return
-
-  echo -e " ${GREEN}󱓏${NC} Swap to branch ${GREEN}$(git_develop_branch)${NC}." 
+  echo -e " ${GREEN}󱓏${NC} Swap to branch ${GREEN}$(current_branch)${NC}." 
   
   git pull origin $(current_branch) > /dev/null 2>&1 || return
   git fetch --all --prune --jobs=10 > /dev/null 2>&1 || return
@@ -36,4 +44,9 @@ function release_push ()
 {
   git push origin $(current_branch) > /dev/null 2>&1 || return
   echo -e " ${GREEN}󱓎${NC} Push release branch ${GREEN}$(current_branch)${NC}." 
+  
+  echo -e " ${YELLOW} ${NC}Go to https://gitlab.spectrumdata.tech/ and merge branch manually."
+
+  git switch $(git_main_branch) > /dev/null 2>&1 || return
+  echo -e " ${GREEN}󱓏${NC} Swap to branch ${GREEN}$(current_branch)${NC}."
 }
