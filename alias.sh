5552cf9df47b06b980285664992046a9cf86e3ad
Александр Колосов <dnwSilver@gmail.com>
2024-01-18 12:54:00 +0500

    Add new alias ghrc, gmod.

--- alias.sh
+++ alias.sh
@@ -10,8 +10,15 @@ alias gcmj="gitmoji -c"                                             # commit bui
 alias grst="git stash && git stash clear"                           # remove all changes
 alias ggpcs="ggp -o ci.skip"                                        # push commit without ci/cd
 alias gfu="ggl && gfa"                                              # pull new commit and branches
+alias ghrc="git reset --hard HEAD && git clean -qfdx"               # ghrc and clean
 alias gtp="create_and_push_tag"                                     # create tag and push with current version
-alias gmod="git merge origin/$(git_develop_branch)"                 # merge with origin dev
+alias gmod='
+CURRENT_BRANCH=$(git_current_branch)
+gswd
+gfu
+gsw $CURRENT_BRANCH
+git merge origin/$(git_develop_branch)
+'                 # merge with origin dev
 alias lfsr='
 git lfs uninstall
 git reset --hard
