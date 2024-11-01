c137fbd106df8f4fed8b9c06ee49ddad35e59aef
Александр Колосов <dnwSilver@gmail.com>
2024-11-01 18:56:48 +0500

    🤡 Modify this shit.

--- poshcontext.sh
+++ poshcontext.sh
@@ -5,12 +5,6 @@ function set_poshcontext() {
         if [ -f "yarn.lock" ]; then PACKET_MANAGER=$(yarn --version); fi
         if [ -f "bun.lockb" ]; then PACKET_MANAGER=$(bun --version); fi
         export MANAGER=$PACKET_MANAGER
-        export REACT=$(node -p "require('./package.json').dependencies?.react || ''" | sed s/\\^//)
         export TS=$(node -p "require('./package.json').dependencies?.typescript || require('./package.json').devDependencies?.typescript" | sed s/\\^//)
     fi
-    if [ -f "global.json" ]; then
-        PROJECT=$(echo $(yq '[ .variables.MAIN_PROJECT_PATH]' .gitlab-ci.yml)/$(yq -r '[ .variables.MAIN_PROJECT_NAME]' .gitlab-ci.yml) | sed -e s/\"//g -e s/-//g -e "s/ //g")
-        export DOTNETPROJ=$(awk -F '[<>]' '/PackageVersion/{print $3}' $PROJECT)
-    fi
-
 }
