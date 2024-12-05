6412e427698d1d81927c87ca8fe7d0c3241fb530
Александр Колосов <dnwSilver@gmail.com>
2024-12-05 18:52:33 +0500

    ✨ Update omp.

--- poshcontext.sh
+++ poshcontext.sh
@@ -5,6 +5,6 @@ function set_poshcontext() {
         if [ -f "yarn.lock" ]; then PACKET_MANAGER=$(yarn --version); fi
         if [ -f "bun.lockb" ]; then PACKET_MANAGER=$(bun --version); fi
         export MANAGER=$PACKET_MANAGER
-        export TS=$(node -p "require('./package.json').dependencies?.typescript || require('./package.json').devDependencies?.typescript" | sed s/\\^//)
+        export TS=$(node -p "require('./package.json').dependencies?.typescript || require('./package.json').devDependencies?.typescript || ''" | sed s/\\^//)
     fi
 }
